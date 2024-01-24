d097b40f6eb5b83a7e03dea14d3fe24053a7f255
ktym4a <shoma@ktym4a.me>
2024-01-24 22:27:07 +0700

    Add rssfeed (#60)
    Close #48 
    
    * Add constants and update page titles and descriptions
    
    * Add @astrojs/rss package dependency

---
+++ src/constants/index.ts
@@ -0,0 +1,2 @@
+export const SITE_NAME = 'ktym4a'
+export const BLOG_DESCRIPTION = 'I write about my thoughts and learnings.'
---
+++ src/pages/blog/rss.xml.ts
@@ -0,0 +1,22 @@
+import { BLOG_DESCRIPTION } from '@/constants/index'
+import { getPosts } from '@/utils/blog'
+import rss from '@astrojs/rss'
+import type { APIContext } from 'astro'
+
+const posts = await getPosts().then((post) => post.reverse())
+
+export function GET(context: APIContext) {
+    const site = new URL('blog', context.site ?? '')
+
+    return rss({
+        title: "ktym4a's Blog",
+        description: BLOG_DESCRIPTION,
+        site,
+        items: posts.map((post) => ({
+            link: `${site}/${post.slug}`,
+            title: post.data.title,
+            description: post.data.description,
+            pubDate: new Date(post.data.publishedDate)
+        }))
+    })
+}
