3de00b4f77d684898c78425567a5dd0f71ece059
ktym4a <shoma@ktym4a.me>
2024-02-18 19:10:36 +0700

    update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,28 @@
-import { defineConfig } from 'astro/config'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import robotsTxt from 'astro-robots-txt'
-import metaTags from 'astro-meta-tags'
-import remarkCodeTitles from 'remark-code-titles'
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import robotsTxt from 'astro-robots-txt';
+import metaTags from 'astro-meta-tags';
+import remarkCodeTitles from 'remark-code-titles';
+
+import pageInsight from "astro-page-insight";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        tailwind(),
-        sitemap(),
-        robotsTxt({
-            sitemap: true
-        }),
-        metaTags()
-    ],
-    site: 'https://ktym4a.me/',
-    base: '/',
-    trailingSlash: 'always',
-    markdown: {
-        shikiConfig: {
-            theme: 'catppuccin-latte',
-            experimentalThemes: {
-                light: 'catppuccin-latte',
-                dark: 'catppuccin-mocha'
-            }
-        },
-        remarkPlugins: [remarkCodeTitles]
-    }
-})
+  integrations: [tailwind(), sitemap(), robotsTxt({
+    sitemap: true
+  }), metaTags(), pageInsight()],
+  site: 'https://ktym4a.me/',
+  base: '/',
+  trailingSlash: 'always',
+  markdown: {
+    shikiConfig: {
+      theme: 'catppuccin-latte',
+      experimentalThemes: {
+        light: 'catppuccin-latte',
+        dark: 'catppuccin-mocha'
+      }
+    },
+    remarkPlugins: [remarkCodeTitles]
+  }
+});
