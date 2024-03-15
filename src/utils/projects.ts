dc986eb2f2f09eb458fb4cc7a0c210b4c5725664
ktym4a <shoma@ktym4a.me>
2024-03-16 01:21:33 +0700

    change to biome

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,17 +1,17 @@
-import { getCollection } from 'astro:content'
+import { getCollection } from "astro:content";
 
-import type { CollectionEntry } from 'astro:content'
-import { sortByDate } from '.'
+import type { CollectionEntry } from "astro:content";
+import { sortByDate } from ".";
 
-export type ProjectsCollection = CollectionEntry<'projects'>
+export type ProjectsCollection = CollectionEntry<"projects">;
 
 export const getProjects = async (): Promise<ProjectsCollection[]> => {
-    const posts = await getCollection('projects')
-    return sortProjects(posts)
-}
+	const posts = await getCollection("projects");
+	return sortProjects(posts);
+};
 
 const sortProjects = (
-    collections: ProjectsCollection[]
+	collections: ProjectsCollection[],
 ): ProjectsCollection[] => {
-    return sortByDate(collections)
-}
+	return sortByDate(collections);
+};
