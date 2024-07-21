ae45c17bfb4ffe1cf37c4f86140a34baad6e14ed
ktym4a <shoma@ktym4a.me>
2024-07-21 20:14:07 +0900

    update: link

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,5 +4,8 @@ export const BLOG_DESCRIPTION = "I write about my thoughts and learnings.";
 
 export const DIARY_LEARNING_DESCRIPTION = "I write about my learnings.";
 
-export const GITHUB_CONTENT_URL =
+export const GITHUB_BLOG_URL =
 	"https://github.com/ktym4a/ktym4a.me/tree/main/src/content/blog/";
+
+export const GITHUB_DIARY_URL =
+	"https://github.com/ktym4a/ktym4a.me/tree/main/src/content/diary/";
